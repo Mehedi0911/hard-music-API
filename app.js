b5dc4945afee5c056c76b0faa4cd1194feb80734
unknown <mmraaj378@gmail.com>
2021-02-09 11:45:09 +0600

    async wait

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const searchSongs = () => {
+const searchSongs = async() => {
     const searchText = document.getElementById("searchText").value;
     const url = `https://api.lyrics.ovh/suggest/:${searchText}`
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySongs(data.data))
+        const res = await fetch(url);
+        const data = await res.json();
+        displaySongs(data.data);
 }
 
 const displaySongs = songs => {
     const songContainer = document.getElementById("songContainer");
-    
+    songContainer.innerHTML = "";
     songs.forEach(song => {
         const songDiv = document.createElement('div');
         songDiv.className = "single-result row align-items-center my-3 p-3";
@@ -31,12 +31,12 @@ const displaySongs = songs => {
     });
 }
 
-const getLyrics = (artist, title) =>{
+const getLyrics = async(artist, title) =>{
     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
 
-    fetch(url)
-    .then(res => res.json())
-    .then(data => didplayLyrics(data.lyrics))
+    const res = await fetch(url);
+    const data = await res.json();
+    didplayLyrics(data.lyrics);
 }
 
 const didplayLyrics = lyrics =>{
